9dfd284f66f96cf95f486e2cca1333596f51b86b
Yibo Jiang <jyb940413@gmail.com>
2018-12-03 01:16:02 -0500

    try new lstm

--- model.py
+++ model.py
@@ -6,8 +6,12 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import argparse
 import os
+from tensorflow.python.ops.rnn import _transpose_batch_time
 
 # attention + bi-directional
+# maml
+# put lstm ouput into lstm
+# reptile + ntm
 def normalized_columns_initializer(std=1.0):
     def _initializer(shape, dtype=None, partition_info=None):
         out = np.random.randn(*shape).astype(np.float32)
@@ -22,7 +26,7 @@ class MetaCluster():
         self.batch_size = config.batch_size
         self.k = 2
         self.num_sequence = 30
-        self.lr = 0.01
+        self.lr = 0.003
         self.model = self.model()
         vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='core')
         vars_ = {var.name.split(":")[0]: var for var in vars}
@@ -51,12 +55,84 @@ class MetaCluster():
 
         return np.expand_dims(data,axis=0),np.expand_dims(labels,axis=0).astype(np.int32)
 
+    def sampling_rnn(self, cell, initial_state, input_, seq_lengths):
+
+        # raw_rnn expects time major inputs as TensorArrays
+        max_time = seq_lengths+1  # this is the max time step per batch
+        inputs_ta = tf.TensorArray(dtype=tf.float32, size=max_time, clear_after_read=False)
+        inputs_ta = inputs_ta.unstack(_transpose_batch_time(input_))  # model_input is the input placeholder
+        output_dim = 2  # the dimensionality of the model's output at each time step
+        input_dim = input_.get_shape()[-1].value +  output_dim # the dimensionality of the input to each time step
+
+        def loop_fn(time, cell_output, cell_state, loop_state):
+            """
+            Loop function that allows to control input to the rnn cell and manipulate cell outputs.
+            :param time: current time step
+            :param cell_output: output from previous time step or None if time == 0
+            :param cell_state: cell state from previous time step
+            :param loop_state: custom loop state to share information between different iterations of this loop fn
+            :return: tuple consisting of
+              elements_finished: tensor of size [bach_size] which is True for sequences that have reached their end,
+                needed because of variable sequence size
+              next_input: input to next time step
+              next_cell_state: cell state forwarded to next time step
+              emit_output: The first return argument of raw_rnn. This is not necessarily the output of the RNN cell,
+                but could e.g. be the output of a dense layer attached to the rnn layer.
+              next_loop_state: loop state forwarded to the next time step
+            """
+            if cell_output is None:
+                # time == 0, used for initialization before first call to cell
+                next_cell_state = initial_state
+                # the emit_output in this case tells TF how future emits look
+                emit_output = tf.zeros([output_dim])
+            else:
+                # t > 0, called right after call to cell, i.e. cell_output is the output from time t-1.
+                # here you can do whatever ou want with cell_output before assigning it to emit_output.
+                # In this case, we don't do anything
+                next_cell_state = cell_state
+                emit_output = cell_output
+
+            # check which elements are finished
+            elements_finished = (time >= seq_lengths)
+            finished = tf.reduce_all(elements_finished)
+
+            # assemble cell input for upcoming time step
+            current_output = emit_output if cell_output is not None else None
+            input_original = inputs_ta.read(time)  # tensor of shape (None, input_dim)
+
+            if current_output is None:
+                # this is the initial step, i.e. there is no output from a previous time step, what we feed here
+                # can highly depend on the data. In this case we just assign the actual input in the first time step.
+                next_in = tf.concat([input_original, tf.zeros([self.batch_size,output_dim])],axis=1)
+            else:
+                # time > 0, so just use previous output as next input
+                # here you could do fancier things, whatever you want to do before passing the data into the rnn cell
+                # if here you were to pass input_original than you would get the normal behaviour of dynamic_rnn
+                next_in = tf.concat([input_original,current_output],axis=1)
+
+            next_input = tf.cond(finished,
+                                 lambda: tf.zeros([self.batch_size, input_dim], dtype=tf.float32),  # copy through zeros
+                                 lambda: next_in)  # if not finished, feed the previous output as next input
+
+            # set shape manually, otherwise it is not defined for the last dimensions
+            next_input.set_shape([None, input_dim])
+
+            # loop state not used in this example
+            next_loop_state = None
+            return (elements_finished, next_input, next_cell_state, emit_output, next_loop_state)
+
+        outputs_ta, last_state, _ = tf.nn.raw_rnn(cell, loop_fn)
+        outputs = _transpose_batch_time(outputs_ta.stack())
+        final_state = last_state
+
+        return outputs, final_state
+
     def model(self):
         sequences = tf.placeholder(tf.float32, [self.batch_size,None, 2])
         labels = tf.placeholder(tf.int32, [self.batch_size,None])
 
         # cell = tf.nn.rnn_cell.BasicLSTMCell(self.n_unints,state_is_tuple=True)
-        cells = [tf.contrib.rnn.BasicLSTMCell(32) for _ in range(2)]
+        cells = [tf.contrib.rnn.BasicLSTMCell(n_unint) for n_unint in [32,32,2]]
         cell = tf.contrib.rnn.MultiRNNCell(cells)
 
         """ Save init states (zeros) """
@@ -73,7 +149,8 @@ class MetaCluster():
 
         """ Define LSTM network """
         with tf.variable_scope('core'):
-            output, states = tf.nn.dynamic_rnn(cell, sequences, dtype=tf.float32, initial_state = cell_init_state)
+            #output, states = tf.nn.dynamic_rnn(cell, sequences, dtype=tf.float32, initial_state = cell_init_state)
+            output, states = self.sampling_rnn(cell, cell_init_state,sequences, self.num_sequence)
             #output, states = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(cell, sequences, dtype=tf.float32, initial_states_fw = cell_init_state)
 
         """ Keep and Clear Op """
@@ -93,7 +170,8 @@ class MetaCluster():
 
         """ Define Policy and Value """
         with tf.variable_scope('core'):
-            policy = tf.layers.dense(output,self.k)
+            #policy = tf.layers.dense(output,self.k)
+            policy = output
 
         """ Define Loss and Optimizer """
         loss = [tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = labels ,logits= policy)),
@@ -107,6 +185,11 @@ class MetaCluster():
 
         miss_rate = tf.minimum(miss_rate_0,miss_rate_1)
 
+        l2 = 0.0005 * sum(
+            tf.nn.l2_loss(tf_var)
+                for tf_var in tf.trainable_variables()
+                if ("core" in tf_var.name)
+        )
         opt = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(loss[0])
         return AttrDict(locals())
 
@@ -115,7 +198,7 @@ class MetaCluster():
         sess.run(model.clear_state_op)
         for epoch_ind in range(100):
             _,_,miss_rate = sess.run([model.keep_state_op,model.opt,model.miss_rate],feed_dict={model.sequences:data,model.labels:labels})
-
+            #miss_rate = sess.run([model.output],feed_dict={model.sequences:data,model.labels:labels})
         print("Epochs{}:{}".format(epoch_ind,miss_rate))
 
     def test(self,data,labels,sess):
@@ -155,7 +238,7 @@ if __name__ == '__main__':
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
             # training
-            for _ in tqdm(range(config.training_exp_num)):
+            for _ in tqdm(range(int(config.training_exp_num/10))):
                 data_list = []
                 labels_list = []
                 for _ in range(config.batch_size):
@@ -164,7 +247,8 @@ if __name__ == '__main__':
                     labels_list.append(labels_one)
                 data = np.concatenate(data_list)
                 labels = np.concatenate(labels_list)
-                metaCluster.train(data,labels,sess)
+                for _ in range(int(10)):
+                    metaCluster.train(data,labels,sess)
 
             # saving models ...
             metaCluster.save_model(sess,config.training_exp_num)
