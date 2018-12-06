720ef9d2ec05da97b0d9d42395c08543f5860be1
Sylvanus <sylvanuszhy@gmail.com>
2018-12-05 20:02:10 -0500

    fea=2

--- model.py
+++ model.py
@@ -31,7 +31,7 @@ class MetaCluster():
         self.batch_size = config.batch_size
         self.k = 2
         self.num_sequence = 100
-        self.fea = 5
+        self.fea = 2
         self.lr = 0.003
         self.model = self.model()
         vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='core')
