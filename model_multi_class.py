bb81dfb122541760b2a58e80ca7a2bda60307c57
Yibo Jiang <jyb940413@gmail.com>
2018-12-09 15:44:17 -0500

    add libaray imports

--- model_multi_class.py
+++ model_multi_class.py
@@ -514,4 +514,4 @@ if __name__ == '__main__':
 
             kmeans = KMeans(n_clusters=3, random_state=0).fit(data_pca)
             print(metaCluster.mutual_info(np.expand_dims(labels,axis=0),np.expand_dims(kmeans.labels_,axis=0)))
-            metaCluster.test(np.expand_dims(data_pca,axis=0),np.expand_dims(labels,axis=0),sess,kmeans)
+            metaCluster.test_compare(np.expand_dims(data_pca,axis=0),np.expand_dims(labels,axis=0),sess,kmeans)
